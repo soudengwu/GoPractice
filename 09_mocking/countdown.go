7c215c1f4f9771e629cb91a3064b03ea6b84bdb3
soudengw <soudengwu@naurix.com>
2023-01-01 16:54:46 +0100

    added configurableSleeper and test for the configurable sleeper.

--- 09_mocking/countdown.go
+++ 09_mocking/countdown.go
@@ -15,28 +15,56 @@ type SpySleeper struct {
 	Calls int
 }
 
-type DefaultSleeper struct{}
+type SpyCountdownOperation struct {
+	Calls []string
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
 
 const finalWord = "GO"
+const sleep = "sleep"
+const write = "write"
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func (s *SpyCountdownOperation) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+func (s *SpyCountdownOperation) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
 
 func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
-}
-
 func Countdown(out io.Writer, startCount int, sleeper Sleeper) {
+
 	for i := startCount; i > 0; i-- {
 		fmt.Fprintln(out, i)
 		sleeper.Sleep()
 	}
+
 	fmt.Fprintln(out, finalWord)
 
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, 6, sleeper)
 }
--- 09_mocking/countdown_test.go
+++ 09_mocking/countdown_test.go
@@ -2,24 +2,55 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
+	"time"
 )
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
+	t.Run("test countdown with mock", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
 
-	Countdown(buffer, 3, spySleeper)
+		Countdown(buffer, 3, &SpyCountdownOperation{})
 
-	got := buffer.String()
-	want := "3\n2\n1\nGO\n"
+		got := buffer.String()
+		want := "3\n2\n1\nGO\n"
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+
+	})
+
+	t.Run("test countdown order", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperation{}
+		Countdown(spySleepPrinter, 3, spySleepPrinter)
+
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
+
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
 	}
-
-	if spySleeper.Calls != 3 {
-		t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
-	}
-
 }
